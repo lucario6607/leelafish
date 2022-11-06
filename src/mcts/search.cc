dbf0d6812f792f6a98a7ce6e9a462bd1e161e1d1
Hans Ekbrand <hans@gpu-master>
2022-11-06 18:27:42 +0100

    fixed a newly introduced locking bug

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -1781,8 +1781,8 @@ const std::shared_ptr<Search::adjust_policy_stats> SearchWorker::PreExtendTreeAn
     int number_of_PVs_added = 0;
     
     while(search_->search_stats_->fast_track_extend_and_evaluate_queue_.size() > 0 &&
-	  search_->search_stats_->Number_of_nodes_added_by_AuxEngine - number_of_added_nodes_at_start < 200 && 
-	  number_of_PVs_added < 200 // don't drag the speed down.
+	  search_->search_stats_->Number_of_nodes_added_by_AuxEngine - number_of_added_nodes_at_start < 50 && 
+	  number_of_PVs_added < 50 // don't drag the speed down.
 	  ){
       // relase the lock, we only needed it to test if to continue or not
       search_->search_stats_->pure_stats_mutex_.unlock_shared();
@@ -1848,7 +1848,7 @@ const std::shared_ptr<Search::adjust_policy_stats> SearchWorker::PreExtendTreeAn
     } // end of while loop
     // If there are nodes left, then fill them with nodes along the highest probability path.    
     number_of_added_nodes_at_start = search_->search_stats_->Number_of_nodes_added_by_AuxEngine;
-    search_->search_stats_->pure_stats_mutex_.lock_shared();
+    search_->search_stats_->pure_stats_mutex_.unlock_shared();
     // Under construction:
     // 1. make root the current node
     // 2. make the best child of the current node the current node
@@ -3063,7 +3063,9 @@ void SearchWorker::DoBackupUpdateSingleNode(
     float q_of_node = node->GetQ(0.0f);
     // float delta = std::abs(q_of_node - q_of_parent); // since they have opposite signs, adding works fine here.
     float delta = std::abs(q_of_node + q_of_parent); // since they have opposite signs, adding works fine here.    
-    if(delta > params_.GetQuiescenceDeltaThreshold() * probability_of_best_path && q_of_node < q_of_parent){
+    if(delta > params_.GetQuiescenceDeltaThreshold() * probability_of_best_path &&
+       q_of_node < q_of_parent &&
+       delta > params_.GetQuiescenceDeltaThreshold() * pow(probability_of_best_path, params_.GetQuiescencePolicyThreshold())){
       if (params_.GetAuxEngineVerbosity() >= 9) LOGFILE << "a quiscence node will be added due to fluctuating eval" << " policy: " << node->GetOwnEdge()->GetP() << " delta: " << delta << " q_of_parent: " << q_of_parent << " q_of_node: " << q_of_node;
       // Create a vector with elements of type Move from root to this node and queue that vector, and queue that vector
       std::vector<lczero::Move> my_moves_from_the_white_side;

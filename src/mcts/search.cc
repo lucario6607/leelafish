fdeff8d2e7345852f17ae283e6ee78e6eb2c33fc
Hans Ekbrand <hans@gpu-master>
2022-12-18 01:31:47 +0100

    trying to fix bug in new part

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -2462,6 +2462,9 @@ bool SearchWorker::PickNodesToExtendTask(Node* node, int base_depth,
 	}
 
 	search_->search_stats_->vector_of_moves_from_root_to_Helpers_preferred_child_node_mutex_.unlock();
+	// read the minimax stuff before chaning locks
+	std::vector<Move> minimax_moves = search_->search_stats_->vector_of_moves_from_root_to_first_minimax_divergence;
+	Node* minimax_node = search_->search_stats_->Leelas_minimax_PV_first_divergence_node;
 	search_->search_stats_->best_move_candidates_mutex.unlock();
 	search_->search_stats_->vector_of_moves_from_root_to_Helpers_preferred_child_node_mutex_.lock();
 
@@ -2490,11 +2493,12 @@ bool SearchWorker::PickNodesToExtendTask(Node* node, int base_depth,
 	    }
 	  } else {
 	    // If there is a minimax divergence, prioritise exploring that
-	    if(search_->search_stats_->vector_of_moves_from_root_to_first_minimax_divergence.size() > 0){
-	      vector_of_moves_from_root_to_boosted_node = search_->search_stats_->vector_of_moves_from_root_to_first_minimax_divergence;
-	      boosted_node = search_->search_stats_->Leelas_minimax_PV_first_divergence_node;
+	    if(minimax_moves.size() > 0){
+	      vector_of_moves_from_root_to_boosted_node = minimax_moves;
+	      boosted_node = minimax_node;
 	      LOGFILE << "Since the helper thinks leelas PV is better than its own, boost something else: now boosting the first diverging node in the minimax PV with " << collision_limit_one << " visits to that node which currently has " << boosted_node->GetN() << " visits.";	      
 	    } else {
+	      
 	      vector_of_moves_from_root_to_boosted_node.pop_back();
 	      if(params_.GetAuxEngineVerbosity() >= 2) LOGFILE << "SearchWorker::PickNodesToExtendTask() Helper likes Leelas PV more than its own, boosting visits to it's parent, and let Leela do her thing.";
 	      boosted_node = search_->search_stats_->Helpers_preferred_child_node_->GetParent();
